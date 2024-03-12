315fb1c413ee8aebdbebd9541635af0fabd4eb7f
Muhammad-Faizan729 <muhammadfaizanfayyaz729@gmail.com>
2024-03-12 12:02:30 -0700

    car model picker updated

--- components/carModelPicker.js
+++ components/carModelPicker.js
@@ -1,45 +1,69 @@
-import React, { useState } from 'react';
-import { View, Text, Modal, TouchableOpacity, ScrollView, FlatList, TextInput, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Modal,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  TextInput,
+  StyleSheet,
+} from "react-native";
 
 const carModelsData = [
   {
-    year: '2020',
+    year: "2020",
     brands: [
       {
-        name: 'Toyota',
-        variants: [
+        name: "Toyota",
+        model: [
           {
-          name:"1.5t"
-          }
+            name: "Corolla",
+            varient: [
+              {
+                name: "1.5t",
+              },
+            ],
+          },
         ],
       },
-      
     ],
   },
-  
+
   // Add more years, brands, and variants as needed
 ];
 
-const CarModelPicker = ({ isVisible, onClose, onSelectYear, onSelectBrand, onSelectVariant }) => {
-  const [selectedYear, setSelectedYear] = useState('');
-  const [selectedBrand, setSelectedBrand] = useState('');
-  const [selectedVariant, setSelectedVariant] = useState('');
+const CarModelPicker = ({
+  isVisible,
+  onClose,
+  onSelectYear,
+  onSelectBrand,
+  onSelectVariant,
+  onSelectModel,
+}) => {
+  const [selectedYear, setSelectedYear] = useState("");
+  const [selectedBrand, setSelectedBrand] = useState("");
+  const [selectedModel, setSelectedModel] = useState("");
+  const [selectedVariant, setSelectedVariant] = useState("");
 
   const handleYearSelect = (year) => {
     setSelectedYear(year);
-    setSelectedBrand('');
-    setSelectedVariant('');
+    setSelectedBrand("");
+    setSelectedVariant("");
   };
 
   const handleBrandSelect = (brand) => {
     setSelectedBrand(brand);
-    setSelectedVariant('');
   };
-
+  const handleModelSelect = (model) => {
+    setSelectedModel(model);
+    setSelectedVariant("");
+  };
   const handleVariantSelect = (variant) => {
     setSelectedVariant(variant);
     onSelectYear(selectedYear);
     onSelectBrand(selectedBrand);
+    onSelectModel(selectedModel);
     onSelectVariant(variant);
     onClose();
   };
@@ -54,59 +78,94 @@ const CarModelPicker = ({ isVisible, onClose, onSelectYear, onSelectBrand, onSel
       <View style={styles.modalContainer}>
         <View style={styles.modalContent}>
           {/* <ScrollView> */}
-            {selectedYear === '' && (
-              <>
-                <Text style={styles.headerText}>Select Year</Text>
-                <FlatList
-                  data={carModelsData}
-                  keyExtractor={(item) => item.year}
-                  renderItem={({ item }) => (
-                    <TouchableOpacity
-                      style={styles.itemButton}
-                      onPress={() => handleYearSelect(item.year)}
-                    >
-                      <Text>{item.year}</Text>
-                    </TouchableOpacity>
-                  )}
-                />
-              </>
-            )}
-            {selectedYear !== '' && selectedBrand === '' && (
-              <>
-                <Text style={styles.headerText}>Select Brand</Text>
-                <FlatList
-                  data={carModelsData.find((data) => data.year === selectedYear).brands}
-                  keyExtractor={(item) => item.name}
-                  renderItem={({ item }) => (
-                    <TouchableOpacity
-                      style={styles.itemButton}
-                      onPress={() => handleBrandSelect(item.name)}
-                    >
-                      <Text>{item.name}</Text>
-                    </TouchableOpacity>
-                  )}
-                />
-              </>
-            )}
-            {selectedBrand !== '' && (
-              <>
-                <Text style={styles.headerText}>Select Variant</Text>
-                <FlatList
-                  data={carModelsData
+          {selectedYear === "" && (
+            <>
+              <Text style={styles.headerText}>Select Year</Text>
+              <FlatList
+                data={carModelsData}
+                keyExtractor={(item) => item.year}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    style={styles.itemButton}
+                    onPress={() => handleYearSelect(item.year)}
+                  >
+                    <Text>{item.year}</Text>
+                  </TouchableOpacity>
+                )}
+              />
+            </>
+          )}
+          {selectedYear !== "" && selectedBrand === "" && (
+            <>
+              <Text style={styles.headerText}>Select Brand</Text>
+              <FlatList
+                data={
+                  carModelsData.find((data) => data.year === selectedYear)
+                    .brands
+                }
+                keyExtractor={(item) => item.name}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    style={styles.itemButton}
+                    onPress={() => handleBrandSelect(item.name)}
+                  >
+                    <Text>{item.name}</Text>
+                  </TouchableOpacity>
+                )}
+              />
+            </>
+          )}
+
+          {selectedBrand !== "" && selectedModel === "" && (
+            <>
+              <Text style={styles.headerText}>Select Model</Text>
+              <FlatList
+                data={
+                  carModelsData
                     .find((data) => data.year === selectedYear)
-                    .brands.find((brandData) => brandData.name === selectedBrand).variants}
-                  keyExtractor={(item) => item}
-                  renderItem={({ item }) => (
-                    <TouchableOpacity
-                      style={styles.itemButton}
-                      onPress={() => handleVariantSelect(item.name)}
-                    >
-                      <Text>{item.name}</Text>
-                    </TouchableOpacity>
-                  )}
-                />
-              </>
-            )}
+                    .brands.find(
+                      (brandData) => brandData.name === selectedBrand
+                    )?.model || []
+                } // Add a check for undefined before accessing models
+                keyExtractor={(item) => item.name}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    style={styles.itemButton}
+                    onPress={() => handleModelSelect(item.name)}
+                  >
+                    <Text>{item.name}</Text>
+                  </TouchableOpacity>
+                )}
+              />
+            </>
+          )}
+
+          {selectedModel !== "" && (
+            <>
+              <Text style={styles.headerText}>Select Variant</Text>
+              <FlatList
+                data={
+                  carModelsData
+                    .find((data) => data.year === selectedYear)
+                    .brands.find(
+                      (brandData) => brandData.name === selectedBrand
+                    )
+                    ?.model.find(
+                      (modelData) => modelData.name === selectedModel
+                    )?.varient || []
+                }
+                keyExtractor={(item) => item.name}
+                renderItem={({ item }) => (
+                  <TouchableOpacity
+                    style={styles.itemButton}
+                    onPress={() => handleVariantSelect(item.name)}
+                  >
+                    <Text>{item.name}</Text>
+                  </TouchableOpacity>
+                )}
+              />
+            </>
+          )}
           {/* </ScrollView> */}
           <TouchableOpacity style={styles.closeButton} onPress={onClose}>
             <Text>Close</Text>
@@ -120,10 +179,10 @@ const CarModelPicker = ({ isVisible, onClose, onSelectYear, onSelectBrand, onSel
 const styles = StyleSheet.create({
   modalContainer: {
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   modalContent: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
     padding: 20,
@@ -131,16 +190,16 @@ const styles = StyleSheet.create({
   },
   headerText: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   itemButton: {
     padding: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    borderBottomColor: "#ccc",
   },
   closeButton: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 10,
   },
 });
